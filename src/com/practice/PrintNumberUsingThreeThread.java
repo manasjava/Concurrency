6fade7892ebff0ba5a5bf1fe7f88333f9ecc35aa
manasranjan.dhal <manasranjan.dhal@172-11-17-73.lightspeed.irvnca.sbcglobal.net>
2019-04-03 10:12:22 +0530

    Print 0 to 9 using three thread sequentially

---
+++ src/com/practice/PrintNumberUsingThreeThread.java
@@ -0,0 +1,48 @@
+package com.practice;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PrintNumberUsingThreeThread {
+
+	public static void main(String[] args) {
+		AtomicInteger num=new AtomicInteger(0);
+		
+		Thread t1=new Thread(new ThreadNumDemo(num,0));
+		Thread t2=new Thread(new ThreadNumDemo(num,1));
+		Thread t3=new Thread(new ThreadNumDemo(num,2));
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
+
+
+class ThreadNumDemo implements Runnable{
+	
+	AtomicInteger num;
+	int threadPosition;
+
+	public ThreadNumDemo(AtomicInteger num, int threadPosition) {
+		this.num=num;
+		this.threadPosition=threadPosition;
+	}
+
+	@Override
+	public void run() {
+		
+		while(num.get() < 10) {
+			synchronized(num) {
+				if (num.get() % 3 == threadPosition) {
+					if (num.get() < 10) {
+						System.out.println(Thread.currentThread().getName()+" : "+num.getAndIncrement());
+					}
+				}
+			}
+		}
+		
+		
+	}
+	
+}
